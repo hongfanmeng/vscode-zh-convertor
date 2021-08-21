e4c30c92029923ebda76795603ce6a8bf8a012d5
hongfanmeng <hongfanmeng@gmail.com>
2021-08-21 14:38:38 +0800

    use opencc-js instead of opencc

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,21 @@
 import * as vscode from "vscode";
-import { OpenCC } from "opencc";
+import * as opencc from "opencc-js";
 
-const COMMANDS: { command: string; configFile: string }[] = [
+const COMMANDS: { command: string; options: opencc.ConverterOptions }[] = [
   {
     command: "vscode-zh-convertor.zh-hans-to-zh-hant",
-    configFile: "s2t.json",
+    options: { from: "cn", to: "hk" },
   },
   {
     command: "vscode-zh-convertor.zh-hant-to-zh-hans",
-    configFile: "t2s.json",
+    options: { from: "hk", to: "cn" },
   },
 ];
 
-function process(textEditor: vscode.TextEditor, configFile: string) {
+function process(
+  textEditor: vscode.TextEditor,
+  options: opencc.ConverterOptions
+) {
   let document: vscode.TextDocument = textEditor.document;
   let selection: vscode.Selection | vscode.Range = textEditor.selection;
   if (selection.isEmpty) {
@@ -23,9 +26,9 @@ function process(textEditor: vscode.TextEditor, configFile: string) {
     );
   }
 
-  const converter: OpenCC = new OpenCC(configFile);
+  const converter: opencc.ConvertText = opencc.Converter(options);
   let text = document.getText(selection);
-  const result = converter.convertSync(text);
+  const result = converter(text);
   textEditor.edit((builder) => {
     builder.replace(selection, result);
   });
@@ -35,7 +38,7 @@ export function activate(context: vscode.ExtensionContext) {
   COMMANDS.forEach((item) => {
     context.subscriptions.push(
       vscode.commands.registerTextEditorCommand(item.command, (textEditor) => {
-        process(textEditor, item.configFile);
+        process(textEditor, item.options);
       })
     );
   });
